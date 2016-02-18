a32a5d210f72f9f5c6f09177cb3b2211168b6243
EvelyneBerrios <evelyne.berrios@hotmail.com>
2016-02-18 14:26:03 -0600

    Programa 5

---
+++ Proyecto 5/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+/*
+Ingresar un numero y determinar su signo si es cero,
+positivo o negativo
+*/
+
+int main()
+{ int num;
+ printf("Ingresar un numero...>");
+ scanf("%d",&num);
+ if(num>0)
+{
+ printf("El numero es Positivo \n");
+}
+ else if (num<0)
+{
+    printf("El nuemro es Negativo \n");
+}
+ else
+{
+    printf("El numero es cero \n");
+}
+ system("PAUSE");
+ return 0;
+}
